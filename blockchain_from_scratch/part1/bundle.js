f5c68337e7cac609767b7724db48221ff013d641
Doug Rosman <drosman@saic.edu>
2020-09-22 07:43:47 -0500

    restructure

--- blockchain_from_scratch/part1/bundle.js
+++ blockchain_from_scratch/part1/bundle.js
@@ -1,75 +1,4 @@
 (function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
-const SHA256 = require('crypto-js/sha256');
-
-class Block {
-    constructor(index, timestamp, data, previousHash = ''){
-        this.index = index;
-        this.timestamp = timestamp;
-        this.data = data;
-        this.previousHash = previousHash;
-        //this.hash = '';
-
-        // implement after creating calculateHash()
-        this.hash = this.calculateHash();
-    }
-
-    calculateHash() {
-        // calculate and store the hash
-        let hash = SHA256(this.index + this.timestamp + JSON.stringify(this.data) + this.previousHash);
-
-        // convert the hash to a string
-        let hashString = hash.toString();
-
-        // return the string
-        return hashString;
-
-    }
-}
-
-class Blockchain {
-    constructor() {
-        // this.chain = [];
-
-        // implement after creating createGenesisBlock
-        this.chain = [this.createGenesisBlock()];
-        
-    }
-
-    createGenesisBlock() {
-        return new Block(0, "09/22/2020", "Genesis Block", "0");
-    }
-
-    // returns latest block in the chain
-    getLatestBlock() {
-        return this.chain[this.chain.length - 1];
-    }
-
-    // adds a block to the chain, accepts a Block object
-    addBlock(newBlock) {
-        // get the hash of the latest block
-        newBlock.previousHash = this.getLatestBlock().hash;
-
-        // calculate the hash of the new block
-        newBlock.hash = newBlock.calculateHash();
-
-        // add the new block to the chain
-        this.chain.push(newBlock);
-    }
-}
-
-// create an instance of the coin
-
-let demoCoin = new Blockchain();
-
-demoCoin.addBlock(new Block(1, "09/22/2020", {amount: 4}));
-demoCoin.addBlock(new Block(2, "09/22/2020", {amount: 494}));
-demoCoin.addBlock(new Block(3, "09/22/2020", {amount: 800}));
-
-console.log(JSON.stringify(demoCoin, null, 4));
-console.log("uhhh cool");
-
-
-},{"crypto-js/sha256":3}],2:[function(require,module,exports){
 (function (global){
 ;(function (root, factory) {
 	if (typeof exports === "object") {
@@ -869,7 +798,7 @@ console.log("uhhh cool");
 
 }));
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
-},{"crypto":76}],3:[function(require,module,exports){
+},{"crypto":76}],2:[function(require,module,exports){
 ;(function (root, factory) {
 	if (typeof exports === "object") {
 		// CommonJS
@@ -1069,7 +998,78 @@ console.log("uhhh cool");
 	return CryptoJS.SHA256;
 
 }));
-},{"./core":2}],4:[function(require,module,exports){
+},{"./core":1}],3:[function(require,module,exports){
+const SHA256 = require('crypto-js/sha256');
+
+class Block {
+    constructor(index, timestamp, data, previousHash = ''){
+        this.index = index;
+        this.timestamp = timestamp;
+        this.data = data;
+        this.previousHash = previousHash;
+        //this.hash = '';
+
+        // implement after creating calculateHash()
+        this.hash = this.calculateHash();
+    }
+
+    calculateHash() {
+        // calculate and store the hash
+        let hash = SHA256(this.index + this.timestamp + JSON.stringify(this.data) + this.previousHash);
+
+        // convert the hash to a string
+        let hashString = hash.toString();
+
+        // return the string
+        return hashString;
+
+    }
+}
+
+class Blockchain {
+    constructor() {
+        // this.chain = [];
+
+        // implement after creating createGenesisBlock
+        this.chain = [this.createGenesisBlock()];
+        
+    }
+
+    createGenesisBlock() {
+        return new Block(0, "09/22/2020", "Genesis Block", "0");
+    }
+
+    // returns latest block in the chain
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    // adds a block to the chain, accepts a Block object
+    addBlock(newBlock) {
+        // get the hash of the latest block
+        newBlock.previousHash = this.getLatestBlock().hash;
+
+        // calculate the hash of the new block
+        newBlock.hash = newBlock.calculateHash();
+
+        // add the new block to the chain
+        this.chain.push(newBlock);
+    }
+}
+
+// create an instance of the coin
+
+let demoCoin = new Blockchain();
+
+demoCoin.addBlock(new Block(1, "09/22/2020", {amount: 4}));
+demoCoin.addBlock(new Block(2, "09/22/2020", {amount: 494}));
+demoCoin.addBlock(new Block(3, "09/22/2020", {amount: 800}));
+
+console.log(JSON.stringify(demoCoin, null, 4));
+console.log("uhhh cool");
+
+
+},{"crypto-js/sha256":2}],4:[function(require,module,exports){
 'use strict';
 
 const asn1 = exports;
@@ -30301,4 +30301,4 @@ function config (name) {
 }
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
-},{}]},{},[1]);
+},{}]},{},[3]);
