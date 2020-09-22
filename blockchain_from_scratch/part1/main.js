3c749f5549b2b405769fc2141c249856ef0b321d
Doug Rosman <drosman@saic.edu>
2020-09-22 05:18:47 -0500

    add more instructions

--- blockchain_from_scratch/part1/main.js
+++ blockchain_from_scratch/part1/main.js
@@ -1,13 +1,69 @@
+const SHA256 = require('crypto-js/sha256');
+
 class Block {
     constructor(index, timestamp, data, previousHash = ''){
         this.index = index;
         this.timestamp = timestamp;
         this.data = data;
-        this.hash = '';
+        this.previousHash = previousHash;
+        //this.hash = '';
+
+        // implement after creating calculateHash()
+        this.hash = this.calculateHash();
     }
 
     calculateHash() {
+        // calculate and store the hash
+        let hash = SHA256(this.index + this.timestamp + JSON.stringify(this.data) + this.previousHash);
+
+        // convert the hash to a string
+        let hashString = hash.toString();
+
+        // return the string
+        return hashString;
 
     }
 }
+
+class Blockchain {
+    constructor() {
+        // this.chain = [];
+
+        // implement after creating createGenesisBlock
+        this.chain = [this.createGenesisBlock()];
+        
+    }
+
+    createGenesisBlock() {
+        return new Block(0, "09/22/2020", "Genesis Block", "0");
+    }
+
+    // returns latest block in the chain
+    getLatestBlock() {
+        return this.chain[this.chain.length - 1];
+    }
+
+    // adds a block to the chain, accepts a Block object
+    addBlock(newBlock) {
+        // get the hash of the latest block
+        newBlock.previousHash = this.getLatestBlock().hash;
+
+        // calculate the hash of the new block
+        newBlock.hash = newBlock.calculateHash();
+
+        // add the new block to the chain
+        this.chain.push(newBlock);
+    }
+}
+
+// create an instance of the coin
+
+let demoCoin = new Blockchain();
+
+demoCoin.addBlock(new Block(1, "09/22/2020", {amount: 4}));
+demoCoin.addBlock(new Block(2, "09/22/2020", {amount: 494}));
+demoCoin.addBlock(new Block(3, "09/22/2020", {amount: 800}));
+
+console.log(JSON.stringify(demoCoin, null, 4));
+console.log("uhhh cool");
 
