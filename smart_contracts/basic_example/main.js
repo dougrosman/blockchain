f20fb08bc5e88462b22f6a60c70d51ee3894ab11
Doog <63171864+doogwebdevgittutorial@users.noreply.github.com>
2020-10-03 22:27:27 -0500

    add cool copy and paste features

---
+++ smart_contracts/basic_example/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+
+show();
+
+$('.copy').on('click', copyToClipboard);
+
+  function show() {
+    const required = "http://127.0.0.1:5400";
+    let location = window.location + "";
+    let firstPart = location.substring(0, 21);
+  
+    if(firstPart === required) {
+      $('.hidden').css('display', 'block');
+    }
+  }
+
+  function copyToClipboard() {
+    let temp = $("<input>");
+    $("body").append(temp);
+    temp.val($(this).text()).select();
+    document.execCommand("copy");
+    temp.remove();
+}
+
+
+
+
+})
