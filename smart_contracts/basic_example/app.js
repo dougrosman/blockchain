5ca139a3dd7b861e6c2414231de3f06096a72272
Doug Rosman <drosman@saic.edu>
2020-10-06 07:54:11 -0500

    ok

--- smart_contracts/basic_example/app.js
+++ smart_contracts/basic_example/app.js
@@ -1,39 +1,38 @@
-$(document).ready(function(){
+
     
     var basicExampleToken;
     var userAccount;
     var currentWallet;
     var dougWallet2 = "0xFd892bB9b092294ec828B4cC4Ad8c621030c92B2";
-    var web3js;
 
     // Checking if Web3 has been injected by the browser (Mist/MetaMask)
-    if (typeof web3 !== 'undefined') {
-        // Use Mist/MetaMask's provider
-        web3js = new Web3(web3.currentProvider);
-        //console.log(web3js);
-        
-        console.log("you have metamask");
-    } else {
-        // Handle the case where the user doesn't have web3. Probably
-        // show them a message telling them to install Metamask in
-        // order to use our app.
-        console.log("you should download metamask");
-    }
+    window.addEventListener("load", function(){
+        if (typeof web3 !== 'undefined') {
+            // Use Mist/MetaMask's provider
+            web3js = new Web3(web3.currentProvider);
+            
+            console.log("you have metamask");
+        } else {
+            // Handle the case where the user doesn't have web3. Probably
+            // show them a message telling them to install Metamask in
+            // order to use our app.
+            console.log("you should download metamask");
+        }
+        startApp();
+    })
     
-    // Now you can start your app & access web3js freely:
-
-    // instantiate contract
+    
 
     function startApp() {
         var myContractAddress = "0x062908cA7AB6478FC1267b97351d1e372090F18c";
         basicExampleToken = new web3js.eth.Contract(myABI, myContractAddress);
-        currentWallet = web3js.eth.accounts;
-        console.log("current wallet: " + currentWallet)
 
-        var accountInterval = setInterval(function() {
+        console.log(web3.eth.accounts);
+        setInterval(function() {
             // Check if account has changed
             if (web3.eth.accounts[0] !== userAccount) {
               userAccount = web3.eth.accounts[0];
+              
               // Call some function to update the UI with the new account
               updateInterface();
             }
@@ -54,7 +53,7 @@ $(document).ready(function(){
         })
     }
     
-    startApp();
+    
 
 
     // getBalance(dougWallet).then(function(result){
@@ -65,4 +64,3 @@ $(document).ready(function(){
 
 
       
-})
