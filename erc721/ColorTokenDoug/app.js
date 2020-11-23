b6f056456e2dbe2c1c19373a2b9752cb627257f7
Doog <63171864+doogwebdevgittutorial@users.noreply.github.com>
2020-11-22 21:27:52 -0600

    it works!

--- erc721/ColorTokenDoug/app.js
+++ erc721/ColorTokenDoug/app.js
@@ -1,5 +1,6 @@
 ethereum.autoRefreshOnNetworkChange = false;
 ethereum.enable();
+
 const provider = new ethers.providers.Web3Provider(window.ethereum);
 const signer = provider.getSigner();
 const contractAddress = "0x8F2580E00De52Dc409651B14268FAf3ca917D1a5";
@@ -8,10 +9,14 @@ const contractABI = [
   "function balanceOf(address account) public view returns (uint256)",
   "function tokenOfOwnerByIndex(address owner, uint256 index) public view returns (uint256)",
   "function totalSupply() public view returns (uint256)",
-  "function tokenByIndex(uint256 index) public view returns (uint256)"
+  "function tokenByIndex(uint256 index) public view returns (uint256)",
+  "function safeTransferFrom(address from, address to, uint256 tokenId) public"
 ];
 
 let address;
+let totalSupply;
+let existingTokens = [];
+let signerTokens = [];
 
 const contract = new ethers.Contract(contractAddress, contractABI, provider);
 const tokenWithSigner = contract.connect(signer);
@@ -21,7 +26,6 @@ main();
 let colorTokens = [];
 
 async function main() {
-
   address = await signer.getAddress();
   console.log(address);
 
@@ -32,11 +36,12 @@ async function main() {
 async function displayOwnedColors(_address) {
   let balance = await contract.balanceOf(_address);
   balance = parseInt(balance);
+  $("#address").text(address);
 
   // stores an array of ints
-  let ownedTokens = await getColorsByOwner(address, balance);
+  signerTokens = await getColorsByOwner(address, balance);
 
-  let ownedColors = idsToColor(ownedTokens);
+  let ownedColors = idsToColor(signerTokens);
   console.log("Owned Color Tokens:");
   console.log(ownedColors);
 
@@ -50,15 +55,83 @@ async function displayOwnedColors(_address) {
     `
       <div class="color-token" id="${colorId}">
         <div class="color-token__tile"></div>
-        <div class="color-token__hex">#abcdef</div>
-      </div>`
+        <div class="color-token__text">
+          <div class="color-token__num">${i+1}</div>
+          <div class="color-token__hex">#abcdef</div>
+        </div>
+      </div>
+      `
 
     $('.color-gallery').append(colorDiv);
 
     $(`#${colorId}`).children('.color-token__tile').css("background", `rgb(${t.r}, ${t.g}, ${t.b})`);
-    $(`#${colorId}`).children('.color-token__hex').text(`${rgbToHex(t.r, t.g, t.b)}`);
+    $(`#${colorId}`).children('.color-token__text').children('.color-token__hex').text(`${rgbToHex(t.r, t.g, t.b)}`);
     
     tokenCounter++;
+  }
+
+  // send tokens
+  $(".color-token").click(function(){
+    let clickedToken = $(".color-token").index($(this));
+    let clickedId = signerTokens[clickedToken];
+    console.log(clickedId);
+
+    $(".send-modal").toggle();
+    $('#token-id').val(`${clickedId}`)
+  });
+}
+
+$('.send-modal__close').click(function(){
+  $('.send-modal').hide();
+})
+$('.send-modal__bg').click(function(){
+  $('.send-modal').hide();
+})
+
+$('.send-modal__btn').click(function(){
+  // verifyAddress();
+  // verifyTokenId();
+  sendToken();
+})
+
+function sendToken() {
+  let recipAddress = $('#recipient-address').val();
+  let token = +$('#token-id').val();
+
+  if(verifyTokenId(token) && verifyAddress(recipAddress)){
+    tokenWithSigner.safeTransferFrom(address, recipAddress, token);
+
+  }
+}
+
+function verifyTokenId(tokenId) {
+  
+  if(signerTokens.indexOf(tokenId) > -1) {
+    console.log("valid tokenId")
+    $('#token-id').css("border", "1px solid #9f9");
+    $('#invalid-token').hide();
+    return true;
+  } else {
+    console.log("invalid tokenId");
+    $('#token-id').css("border", "1px solid #f99");
+    $('#invalid-token').show();
+    return false;
+  }
+}
+
+function verifyAddress(addr) {
+  
+  let addressRegex = /^0x([A-Fa-f0-9]{40})$/
+  if(addressRegex.test(addr)) {
+    console.log("valid address")
+    $('#recipient-address').css("border", "1px solid #9f9");
+    $('#invalid-address').hide();
+    return true;
+  } else {
+    console.log("invalid address");
+    $('#recipient-address').css("border", "1px solid #f99");
+    $('#invalid-address').show();
+    return false;
   }
 }
 
@@ -72,7 +145,7 @@ async function getColorsByOwner(_address, _balance) {
     currToken = parseInt(currToken);
     ids.push(currToken);
   }
-  return ids;
+  return ids.sort();
 }
 
 // convert IDs to RGB Colors, return an array of array[r,g,b]
@@ -100,17 +173,19 @@ function rgbToHex(r, g, b) {
   return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
 }
 
-
-async function checkExisting() {
-  let preownedTokens = [];
-  let totalSupply = await contract.totalSupply();
+async function loadExisting() {
+  totalSupply = await contract.totalSupply();
   totalSupply = parseInt(totalSupply);
-
   for(let i = 0; i < totalSupply; i++) {
     let currToken = await contract.tokenByIndex(i);
-    preownedTokens.push(parseInt(currToken));
+    existingTokens.push(parseInt(currToken));
   }
-  console.log("preowned tokens: " + preownedTokens);
+  console.log(existingTokens);
+  return existingTokens;
+}
+
+
+function checkExisting(_existingTokens) {
 
   // generate a random color
   let randomR = Math.floor(Math.random()*256)
@@ -119,7 +194,7 @@ async function checkExisting() {
 
   let myColor = 1000000000 + randomR*1000000 + randomG*1000 + randomB;
 
-  while(preownedTokens.indexOf(myColor) > 0)
+  while(_existingTokens.indexOf(myColor) > 0)
   {
     randomR = Math.floor(Math.random()*256)
     randomG = Math.floor(Math.random()*256)
@@ -136,25 +211,58 @@ async function checkExisting() {
 
 async function mintColor() {
 
-  shouldReward = await checkExisting();
-  let id = "" + shouldReward;
+  let timeOut;
+  var interval = 250;
+  let cycle = true;
+  $('#loading').show();
+  $('#generate-color').hide();
 
-  let r = parseInt(id.substr(1, 3));
-  let g = parseInt(id.substr(4, 3));
-  let b = parseInt(id.substr(7, 3));
-
-  $('.minted-color__tile').css("background", `rgb(${r}, ${g}, ${b})`)
-  $('.minted-color__hex').text(`${rgbToHex(r, g, b)}`);
+  setInterval(function(){
+    if(cycle) {
+      let randR = Math.floor(Math.random()*256);
+      let randG = Math.floor(Math.random()*256);
+      let randB = Math.floor(Math.random()*256);
+      $('.minted-color__tile').css("background", `rgb(${randR}, ${randG}, ${randB})`)
+      $('.minted-color__hex').text(`${rgbToHex(randR, randG, randB)}`);
+    }
+  }, interval);
   
 
-  if(shouldReward) {
-    tokenWithSigner.awardItem(address, shouldReward);
-    shouldReward = false;
+  if(existingTokens.length == 0) {
+    existingTokens = await loadExisting();
+    timeOut = 0;
+  } else {
+    timeOut = 10000;
   }
+  
+  setTimeout(function(){
+    cycle = false;
+    $('#loading').hide();
+    $('#generate-color').css("display", "block");
+  
+    let rewardId = checkExisting(existingTokens);
+    let id = "" + rewardId;
+
+    let r = parseInt(id.substr(1, 3));
+    let g = parseInt(id.substr(4, 3));
+    let b = parseInt(id.substr(7, 3));
+
+    $('.minted-color__tile').css("background", `rgb(${r}, ${g}, ${b})`)
+    $('.minted-color__hex').text(`${rgbToHex(r, g, b)}`);
+    
+    tokenWithSigner.awardItem(address, rewardId);
+  
+  }, timeOut);
+  
 }
 
+
+
+
+
+
 let button = document.getElementById("generate-color")
-let shouldReward = 0;
+
 button.addEventListener("click", async function(){
   // generate color, initiate transaction to mint ownership of color
   await mintColor();
