903a7f476389232b056a6c3454d6ac3d0481ac5d
Doog <63171864+doogwebdevgittutorial@users.noreply.github.com>
2020-11-27 18:11:40 -0600

    add contractURI

--- erc721/URITests/contracts/ContractURI.sol
+++ erc721/URITests/contracts/ContractURI.sol
@@ -5,15 +5,30 @@
 
 // Goal: Implement a basic NFT that allows ownership of a color
 
+
 pragma solidity ^0.6.0;
 
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Counters.sol";
 
-contract ColorToken is ERC721 {
-    constructor() public ERC721("ColorToken", "COLR") {}
-    function awardItem(address player, uint256 tokenId) public returns (uint256){
-        uint256 newItemId = tokenId;
+
+contract ContractURI is ERC721 {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
+    
+    constructor() public ERC721("ContractURI", "CURI") {}
+    
+    
+    function awardItem(address player, string memory tokenURI)
+        public
+        returns (uint256)
+    {
+        _tokenIds.increment();
+
+        uint256 newItemId = _tokenIds.current();
         _mint(player, newItemId);
+        _setTokenURI(newItemId, tokenURI);
+
         return newItemId;
     }
 }
